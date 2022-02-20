68b53943a252e5358d312ae4602885316ac95165
ahmed khaled <ahmedkhmo69@gmail.com>
2022-02-20 15:10:27 +0200

    add all dependancies and make the structure packages for all modules

--- app/src/main/java/com/assignment/clientapp/presentation/ui/views/activities/MainActivity.kt
+++ app/src/main/java/com/assignment/clientapp/presentation/ui/views/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.assignment.clientapp
+package com.assignment.clientapp.presentation.ui.views.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.assignment.clientapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
